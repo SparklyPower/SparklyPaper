d1ef82909d5db5e2042c5717b4f1504801cecf91
MrPowerGamerBR <git@mrpowergamerbr.com>
2024-11-25 13:40:19 -0300

    Update to Paper 1.21.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     `maven-publish`
 
     // In general, keep this version in sync with upstream. Sometimes a newer version than upstream might work, but an older version is extremely likely to break.
-    id("io.papermc.paperweight.patcher") version "1.7.1"
+    id("io.papermc.paperweight.patcher") version "1.7.5"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
